d40ad12615e29448895b1af9e2f22e892f3e3cc3
odeclasonarres <odeclasonarres@gmail.com>
2019-03-26 11:49:02 +0100

    Actividades NPDEV

--- Ejercicios/NPDEV/numberPeopleSpace.py
+++ Ejercicios/NPDEV/numberPeopleSpace.py
@@ -2,3 +2,14 @@
 # Crear una app para saber el número de personas que hay en el espacio. Podéis buscar la información en la siguiente URL:
 # http://open-notify.org/Open-Notify-API/
 # Subir el archivo a vuestro repositorio en GitHub.
+import urllib.parse
+import requests
+
+url='http://api.open-notify.org/astros.json'
+json_data = requests.get(url).json()
+number=int(json_data['number'])
+print("Hay "+str(number)+" astronautas.")
+print("Sus nombres son: ")
+#print(json_data['people'])
+for i in json_data['people']:
+    print("\t"+i['name'])
--- Ejercicios/NPDEV/sunsetSunrise.py
+++ Ejercicios/NPDEV/sunsetSunrise.py
@@ -6,3 +6,14 @@
 # Mostrar las horas para una latitud y longitud concretas (que no sea la del ejemplo; por ejemplo, donde vivís).
 # Incluir un print con texto (lo que queráis), con los valores de latitud y longitud, y con las horas obtenidas.
 # Subir el archivo a vuestro repositorio en GitHub.
+
+import urllib.parse
+import requests
+
+coordenadas=[str(37.1881),str(-3.6066)]
+url='https://api.sunrise-sunset.org/json?lat='+coordenadas[0]+'&lng='+coordenadas[1]
+json_data = requests.get(url).json()
+ciudad='Granada'
+salida=json_data['results']['sunrise']
+puesta=json_data['results']['sunset']
+print("Mi ciudad es: "+ciudad+". Sus coordenadas son: \n\t-Latitud: "+coordenadas[0]+"\n\t-Longitud: "+coordenadas[1]+"\nLa hora de salida del sol: "+salida+"\nLa hora de puesta del sol: "+puesta)
